2f7c41d5fb680a8a95ee51afdffefa40d32e0ac2
HomerusJa <jakob@schluse.com>
2024-05-11 12:43:37 +0200

    fix: Wrapped handling in TimerSingle.h was always triggering

--- src/TimerSingle.cpp
+++ src/TimerSingle.cpp
@@ -3,44 +3,46 @@
 namespace Timers {
 
 TimerSingle::TimerSingle(uint32_t interval) {
-  _interval = interval;
-  _nextTime = millis() + interval;
+    _interval = interval;
+    _nextTime = millis() + interval;
 }
 
 bool TimerSingle::isTime() {
-  uint32_t now = millis();
-  // This statement handles the case where the timer has wrapped around
-  bool wrapped = _nextTime < _interval;
-  if (now >= _nextTime || !wrapped) {
-    _nextTime = now + _interval;
-    return true;
-  }
-  return false;
+    uint32_t now = millis();
+    // This statement handles the case where the timer has wrapped around
+    bool wrapped = _nextTime < _interval;
+    if (now >= _nextTime || wrapped) {
+        while (_nextTime < now) {
+            _nextTime += _interval;
+        }
+        return true;
+    }
+    return false;
 }
 
 uint32_t TimerSingle::getInterval() { return _interval; }
 
 uint32_t TimerSingle::getRemainingTime() {
-  uint32_t now = millis();
+    uint32_t now = millis();
 
-  bool wrapped = _nextTime < _interval;
+    bool wrapped = _nextTime < _interval;
 
-  if (wrapped) {
-    return UINT32_MAX - now + _nextTime;
-  } else {
-    return _nextTime > now ? _nextTime - now : 0;
-  }
+    if (wrapped) {
+        return UINT32_MAX - now + _nextTime;
+    } else {
+        return _nextTime > now ? _nextTime - now : 0;
+    }
 }
 
 void TimerSingle::setInterval(uint32_t interval, bool reset) {
-  if (reset) {
-    _interval = interval;
-    _nextTime = millis() + interval;
-  } else {
-    uint32_t now = millis();
-    uint32_t remainingTime = (_nextTime > now) ? _nextTime - now : 0;
-    _interval = interval;
-    _nextTime = now + interval - remainingTime;
-  }
+    if (reset) {
+        _interval = interval;
+        _nextTime = millis() + interval;
+    } else {
+        uint32_t now = millis();
+        uint32_t remainingTime = (_nextTime > now) ? _nextTime - now : 0;
+        _interval = interval;
+        _nextTime = now + interval - remainingTime;
+    }
 }
-} // namespace Timers
+}  // namespace Timers
